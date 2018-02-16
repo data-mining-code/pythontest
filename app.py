f1bd2ddcf5e4459c90420f94b9fc98ea42870ddc
8BitJonny <timon.christiansen@code.berlin>
2018-02-16 15:48:23 +0100

    extented answers by 100 and slightly changed intents. Improved string processing

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+from collections import Counter
 import os
 import socket
 import numpy as np # linear algebr
@@ -8,25 +9,27 @@ import json
 
 app = Flask(__name__)
 
-fifa = pd.read_csv("CompleteDataset.csv", skipinitialspace=True)
-fifa = fifa.drop('Unnamed: 0', axis=1)
-
 intentfile = open('intents.json','r')	# Open Intent Json File and read it into a Dictionary
 intents = json.load(intentfile)
 intentfile.close()
 
 @app.route("/")
 def hello():
-    input = request.args.get('input')		# Get Input from Broker												
-	input_list = input.lower().split(" ")					# Make the input lowercase and split it into the words
-	if '?' in input_list[-1] and len(input_list[-1]) > 1: input_list[-1] = input_list[-1][:-1]		# checks if in the last element a word and a '?' and removes the ?
-	query = {} 
+	input = request.args.get('input')		# Get Input from Broker
+	input_list = input.lower().split(" ")	# Make the input lowercase
+	query = {}
 	for word in input_list:
 		for intent in intents:
 			for iword in intent['words']:
-				if word == iword:
-					query[intent['tag']] = word
-	return json.dumps(query) 
+				test = iword.split(" ")
+				for iw in test:
+					if word == iw and intent['tag'] in query.keys():
+						query[intent['tag']].append(iword)
+					elif word == iw:
+						query[intent['tag']] = [iword] 
+	for key in query.keys():
+		query[key] = Counter(query[key]).most_common()[0][0]
+	return json.dumps(query)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
